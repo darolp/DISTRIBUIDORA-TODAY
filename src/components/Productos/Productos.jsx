c497568e7c29d494e6f2bd0362796ca683d81747
darolp <dariopizzi12@gmail.com>
2023-06-01 16:23:47 -0300

    cambios

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -5,47 +5,47 @@ import ItemDetail from '../ItemDetail/ItemDetail';
 
 const Productos = () => {
 
-    
-    const [estado , setestado] = useState('Pastas');
-    const [productosVisual , setproductosVisual] = useState ([]);
 
-   
+    const [estado, setestado] = useState('Pastas');
+    const [productosVisual, setproductosVisual] = useState([]);
 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         fetch(`../../json/${estado}.json`)
-        .then(response =>response.json())
-        .then(data =>{            
-            const productosAux =  <ItemDetail productos={data}/>
-            setproductosVisual(productosAux)
-        })
-    },[estado])
+            .then(response => response.json())
+            .then(data => {
+                const productosAux = <ItemDetail productos={data} />
+                setproductosVisual(productosAux)
+            })
+    }, [estado])
 
     const handleCategoryClick = (category) => {
         setestado(category);
-      };
+    };
     return (
         <>
-        <div className="ProductosContainer">
-            <div className='navProductos'>
-                <ul>
-                    <li className={({isActive}) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Pastas')}>Pastas</li>
-                    <li className={({isActive}) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Panificados')}>Panificados</li>
-                    <li className={({isActive}) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('EmpanadasyTartas')}>Empanadas y Tartas</li>
-                    <li className={({isActive}) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Helados')}>Helados</li>
-                    <li className={({isActive}) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Insumos')}>Insumos</li>
-                </ul>
+            <div className="ProductosContainer">
+                <div className='navProductos'>
+                    <ul>
+                        <li className={({ isActive }) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Pastas')}>Pastas</li>
+                        <li className={({ isActive }) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Panificados')}>Panificados</li>
+                        <li className={({ isActive }) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('EmpanadasyTartas')}>Empanadas y Tartas</li>
+                        <li className={({ isActive }) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Helados')}>Helados</li>
+                        <li className={({ isActive }) => isActive ? 'active' : ""} onClick={() => handleCategoryClick('Insumos')}>Insumos</li>
+                    </ul>
+                </div>
+                <div className='productosView'>
+                    {productosVisual}
+                </div>
             </div>
-            <div className='productosView'>
-                {productosVisual}
-            </div>
-        </div>
         </>
-    );     
-    }
- 
-    
+    );
+}
 
-    
+
+
+
 
 
 export default Productos;
