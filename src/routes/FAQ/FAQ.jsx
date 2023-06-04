babf2272d8044567788525917b0dc01557aac847
Tuky <rodri_cjs22@hotmail.com>
2023-06-04 18:36:48 -0300

    preguntas frecuentes

--- src/routes/FAQ/FAQ.jsx
+++ src/routes/FAQ/FAQ.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import PreguntasFrecuentes from '../../components/PreguntasFrecuentes/PreguntasFrecuentes';
 
 function FAQ() {
   return (
     <>
       <Navbar />
-      <Link to={'/'}>volver</Link>
+      <PreguntasFrecuentes/>
       <Footer />
     </>
   )
