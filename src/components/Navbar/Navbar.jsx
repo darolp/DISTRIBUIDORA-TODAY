3718fa8033c1324f34e7b359be33c21845660db2
Tuky <rodri_cjs22@hotmail.com>
2023-06-05 19:24:45 -0300

    nav mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,11 @@ import logo from '../../assets/imgs/logo.png'
 import { NavLink } from 'react-router-dom'
 function Navbar() {
   const [navbarStyle, setNavbarStyle] = useState('navbar');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   useEffect(() => {
     // si hace scroll cambia el estilo del navbar
     const handleScroll = () => {
@@ -26,14 +31,16 @@ function Navbar() {
     <header>
       <nav className={navbarStyle}>
         <NavLink to='/'>
-          <img src={logo} alt="Arcoiris con la palabra today dentro" />
+          <img className="Arcoiris" src={logo} alt="Arcoiris con la palabra today dentro" />
         </NavLink>
-        <ul>
+        <p className='tituloMobile'>TODAY</p>
+        <ul className={`navbarUl ${isMenuOpen ? 'menuOpen' : ''}`}>
           <li><NavLink to='/nosotros' className={({isActive}) => isActive ? 'active' : ""}>Nosotros</NavLink></li>
           <li><NavLink to='/productos' className={({isActive}) => isActive ? 'active' : ""}>Productos</NavLink></li>
           <li><NavLink to='/contact' className={({isActive}) => isActive ? 'active' : ""}>Contacto</NavLink></li>
           <li><NavLink to='/FAQ' className={({isActive}) => isActive ? 'active' : ""}>Preguntas frecuentes</NavLink></li>
         </ul>
+        <i className="fa-solid fa-bars menu" onClick={toggleMenu}></i>
       </nav>
     </header>
   )
