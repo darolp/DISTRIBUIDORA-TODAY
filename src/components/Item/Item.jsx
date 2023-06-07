485e05c1f90293cef58f245b6fd304eedf1a7611
Tuky <rodri_cjs22@hotmail.com>
2023-06-07 12:21:42 -0300

    arreglando responsive mobile

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 
 
 const Item = ({data}) => {  
+    const buttonClass = data.texto ? 'boton' : 'boton2';
+    const cardClass = data.texto ? 'card' : 'card2';
        
     return (
         
-              <div className="card" >
+              <div className={cardClass} >
                     <div className='card-img'>
                         <img src={`../src/assets/imgs/${data.img}.jpg`} className="" alt="..." />
                     </div>                    
                     <div className="cardInfo">
                         <h5 className="card-title">{data.nombre}</h5>   
-                        <p>{data.texto}</p>                 
-                        <Link  to={"ws"}><button className='boton'><i className="fa-brands fa-whatsapp whatsapp"></i> Hacé tu pedido</button></Link>
+                        {data.texto && <p>{data.texto}</p>}                 
+                        <Link  to={"ws"}><button className={buttonClass}><i className="fa-brands fa-whatsapp whatsapp"></i> Hacé tu pedido</button></Link>
                     </div>
                </div>   
         
